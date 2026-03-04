9f08cd837d60e542a50b9b50ed1e9e892ec8d905
Mauro Quarroz <quarrozmauro@gmail.com>
2026-03-04 14:39:07 -0300

    detalles

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom"
-import "./itemListContainer.css"
+import "./ItemListContainer.css"
 import { useEffect, useState } from "react"
 import ItemList from "../ItemList/ItemList"
 import { db } from "../../services/config"
